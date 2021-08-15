44865e1486c2b3c9121d32bceb2a8eb0e48ca9a3
小翼 <zhijun@furry.top>
2021-08-16 02:27:24 +0400

    修正评论数据库模型的错误语法

--- sql/t_comments.py
+++ sql/t_comments.py
@@ -4,11 +4,13 @@ from sqlalchemy.sql import func
 
 class Comments(db.Model):
     __tablename__ = "comments"
-    id = db.Column(Integer, primary_key=True, comment='文档号')
-    arch_id = db.Column(db.Integer,
-                        nullable=False,
-                        comment='文章ID',
-                        ForeignKey("archives.id"))
+    id = db.Column(db.Integer, primary_key=True, comment='文档号')
+    arch_id = db.Column(
+        db.Integer,
+        db.ForeignKey("archives.id"),
+        nullable=False,
+        comment='文章ID',
+    )
     comment = db.Column(db.String(64), nullable=False, comment='评论')
     create_time = db.Column(db.DateTime,
                             nullable=False,
@@ -19,4 +21,4 @@ class Comments(db.Model):
                             server_default=func.now(),
                             comment='修改时间',
                             onupdate=func.now())
-    arch = relationship("Archives", backref="ref_comments")
+    arch = db.relationship("Archives", backref="ref_comments")
