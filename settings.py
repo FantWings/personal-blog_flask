16eb7b80e9d668a302aa176a7bf09013c47045ba
小翼 <zhijun@furry.top>
2021-08-16 04:55:00 +0400

    改为使用INI文件配置

--- settings.py
+++ settings.py
@@ -1,22 +1,53 @@
-from logging import warn
-import os
+from os import path, urandom
 from datetime import timedelta
+
 from utils.log import log
+import configparser
+
+base_dir = path.abspath(path.dirname(__file__))
+ini = configparser.ConfigParser()
+ini.read("config.ini", encoding="utf-8")
 
 
-class Config(object):
-    # SQLALCHEMY配置
-    SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}:{}/{}".format(
-        os.getenv("SQL_USER", "root"),
-        os.getenv("SQL_PASS", None),
-        os.getenv("SQL_HOST", "127.0.0.1"),
-        os.getenv("SQL_PORT", 3306),
-        os.getenv("SQL_BASE", "blog"),
+class FlaskConfig(object):
+    """FLASK配置"""
+
+    SECRET_KEY = urandom(24)
+    PERMANENT_SESSION_LIFETIME = timedelta(days=31)
+
+    """ SMTP配置 """
+    SMTP_USER = ini.get("smtp", "user")
+    SMTP_PASS = ini.get("smtp", "pass")
+    SMTP_HOST = ini.get("smtp", "host")
+    SMTP_PORT = ini.get("smtp", "port")
+
+    """数据库配置"""
+    if ini.get("db", "mode") == "mysql":
+        # 拼接SQL URI
+        database_uri = "mysql://%s:%s@%s:%s/%s" % (
+            ini.get("db", "user"),
+            ini.get("db", "pass"),
+            ini.get("db", "host"),
+            ini.get("db", "port"),
+            ini.get("db", "base"),
+        )
+    else:
+        database_uri = False
+    SQLALCHEMY_DATABASE_URI = database_uri or "sqlite:///" + path.join(
+        base_dir, "sqlite.db"
     )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+    JSON_AS_ASCII = False
+
+    """Redis配置"""
+    REDIS_HOST = ini.get("redis", "host")
+    REDIS_PORT = ini.get("redis", "port")
+    REDIS_DB = ini.get("redis", "db")
+    REDIS_EXPIRE = ini.get("redis", "expire")
 
     log(
         """
-
     ********************************************************
                       YOU ARE IN DEBUG MODE
     ********************************************************
@@ -28,18 +59,5 @@ class Config(object):
     """,
         "warn",
     )
+
     log("SQL_URI is: {}".format(SQLALCHEMY_DATABASE_URI), "debug")
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # 禁用ASCII编码
-    JSON_AS_ASCII = False
-
-    # 设置SESSION安全密钥
-    SECRET_KEY = os.urandom(24)
-
-    # 设置SESSION有效期
-    PERMANENT_SESSION_LIFETIME = timedelta(days=31)
-
-    # 开启SQLALCHEMY哆嗦模式
-    SQLALCHEMY_ECHO = os.getenv("SQL_ECHO", False)
