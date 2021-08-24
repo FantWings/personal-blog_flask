61cc63026abe314c7ea2a3e8e58d0f9d1fd19ba9
小翼 <zhijun@furry.top>
2021-08-24 23:30:00 +0400

    撰写简单的预览文本切割，补全博文列表的作者信息

--- crud/archives.py
+++ crud/archives.py
@@ -11,29 +11,27 @@ from .auth import loginRequired
 
 
 def queryArchiveList():
-    archives = t_archives.query.with_entities(
-        t_archives.id,
-        t_archives.cover_image,
-        t_archives.time_for_read,
-        t_archives.title,
-        t_archives.update_time,
-        t_archives.views,
-    ).all()
+    archives = t_archives.query.all()
     # tags = t_tags.query.with_entities(t_tags.name).all()
     # ref = t_tags_archives.query.all()
     # print(ref)
     data = []
     for result in archives:
+        # print(result.content.split('\n\n'))
         data.append(
             {
                 "cover_image": result.cover_image,
                 "id": result.id,
-                # "preview": result.preview,
+                "preview": result.content[:30].split('\n\n')[1],
                 "time_for_read": result.time_for_read,
                 "title": result.title,
                 "update_time": toTimeStamp(result.update_time),
                 "views": result.views,
-                # "tags": tags
+                "content": result.content,
+                "author": {
+                    "username": result.author.username,
+                    "avatar": result.author.avatar,
+                },
             }
         )
     return {"data": data}
